635980b790792f5549a629d303b6aba6ab9364f4
Mao Quan <shmao288@gmail.com>
2020-09-19 22:54:27 +0800

    fix

--- tools/init.sh
+++ tools/init.sh
@@ -24,7 +24,7 @@ check_status() {
 
 
 update_files() {
-   /Library/Frameworks/Python.framework/Versions/3.5/bin/python3 _scripts/py/init_all.py
+   python _scripts/py/init_all.py
   find . | grep -E "(__pycache__|\.pyc|\.pyo$)" | xargs rm -rf
 }
 
